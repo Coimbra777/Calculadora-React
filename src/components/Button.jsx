bf11eed3c731ac51be31e28c6d39c6106308cd1b
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-08-14 08:35:20 -0300

    Finalização dos estilos da Calculadora

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import "./Button.css";
 
 const Button = (props) => {
-  // Certifique-se de que a linha abaixo inclua o "return"
-  return <button className="button">{props.label}</button>;
+  // let classes = "button";
+  // classes += props.operation ? "operation" : "";
+  // classes += props.double ? "double" : "";
+  // classes += props.triple ? "triple" : "";
+  const buttonClasses = `button
+  ${props.operation ? "operation" : ""}
+  ${props.double ? "double" : ""}
+  ${props.triple ? "triple" : ""}`;
+
+  return (
+    <button
+      onClick={() => {
+        return props.click(props.label);
+      }}
+      className={buttonClasses}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
